295134ee43bf4a4ccf0f4be3c58d6cd8ee31e01c
GustavoSasaki <spamargsr@gmail.com>
2025-01-02 15:10:01 -0300

    adding jump strength as an stat

--- api.lua
+++ api.lua
@@ -3,7 +3,7 @@ local table_insert = table.insert
 local function getNotNil(value,valueType,defaultValue)
     if value ~= nil and type(value) == valueType then
         if type(value) == "number" then
-            return value/100
+            return value/100 -1.0
         end
         return value
     end
@@ -14,6 +14,15 @@ end
 local function clean_character_stats(cs) 
     cs['name'] = getNotNil(cs['name'], "string", "Untitled")
     cs['swimming_speed'] = getNotNil(cs['swimming_speed'], "number", 1.0)
+
+    local allJumpsStrength = getNotNil(cs['jump_strength'], "number", 1.0)
+    cs['single_jump_strength'] = getNotNil(cs['single_jump_strength'], "number", allJumpsStrength)
+    cs['double_jump_strength'] = getNotNil(cs['double_jump_strength'], "number", allJumpsStrength)
+    cs['triple_jump_strength'] = getNotNil(cs['triple_jump_strength'], "number", allJumpsStrength)
+    cs['back_flip_strength'] = getNotNil(cs['back_flip_strength'], "number", allJumpsStrength)
+    cs['side_flip_strength'] = getNotNil(cs['side_flip_strength'], "number", allJumpsStrength)
+    cs['long_jump_strength'] = getNotNil(cs['long_jump_strength'], "number", allJumpsStrength)
+    cs['kick_jump_strength'] = getNotNil(cs['kick_jump_strength'], "number", allJumpsStrength)
 end
 
 
