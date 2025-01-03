80ea19dd3dc05c36f2931e3c3eff124c38240c6b
GustavoSasaki <spamargsr@gmail.com>
2025-01-02 23:05:40 -0300

    adding example for character stats

--- initial-characterstats-table.lua
+++ initial-characterstats-table.lua
@@ -1,5 +1,54 @@
 initialCharacterStatsTable = {{
-    name = "Iono",
-    swimming_speed = 500,
-    jump_strength = 110
+    -- bigger and faster long jump
+    name = "Hatsune Miku",
+    long_jump_max_velocity = 150,
+    long_jump_velocity_multiplier = 200,
+    long_jump_strength = 125
+}, {
+    -- float character
+    name = "Sackboy",
+    gravity = 95
+}, {
+    -- heavy character with better back/side flip
+    name = "Peter Griffin",
+    gravity = 115,
+    back_flip_strength = 120,
+    side_flip_strength = 120
+}, {
+    -- float character with big long jump
+    name = "Osaka",
+    gravity = 90,
+    long_jump_strength = 120
+}, {
+    -- character with big triple jump/fast long jump
+    name = "Tomo",
+    long_jump_velocity_multiplier = 300,
+    long_jump_max_velocity = 150
+}, {
+    -- character with a faster and upward trajectory dive
+    name = "Kagura",
+    dive_y_vel = 20,
+    dive_velocity = 120,
+    dive_max_velocity = 125
+}, {
+    -- character with big kick
+    name = "Chiyo",
+    gravity = 140,
+    kick_jump_strength = 250,
+    triple_jump_strength = 130,
+    swimming_speed = 95
+}, {
+    -- character with bigger jumps
+    name = "Sakaki",
+    swimming_speed = 400,
+    jump_strength = 125
+}, {
+    -- weird character with bad normal jump, but good back/side/long jumps
+    name = "Kaori",
+    single_jump_strength = 80,
+    double_jump_strength = 80,
+    triple_jump_strength = 80,
+    back_flip_strength = 130,
+    side_flip_strength = 130,
+    long_jump_strength = 130
 }}
