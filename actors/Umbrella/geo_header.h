19c99128bfe669c421aa5c5e761ea806e717565b
GustavoSasaki <spamargsr@gmail.com>
2026-03-02 22:13:34 -0300

    adding umbrella model

---
+++ actors/Umbrella/geo_header.h
@@ -0,0 +1,8 @@
+extern const GeoLayout Umbrella_geo[];
+extern Lights1 umbrella_mat1_f3d_001_lights;
+extern u8 umbrella_big_stick_t64_rgba16[];
+extern Vtx umbrella_umbrellahand_mesh_layer_1_vtx_0[425];
+extern Gfx umbrella_umbrellahand_mesh_layer_1_tri_0[];
+extern Gfx mat_umbrella_mat1_f3d_001[];
+extern Gfx umbrella_umbrellahand_mesh_layer_1[];
+extern Gfx umbrella_material_revert_render_settings[];
